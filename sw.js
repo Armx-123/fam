3f73ced059716db529c4fc078361e3ec8d541c18
Armx <armx94522@gmail.com>
2026-02-28 16:27:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (e) => {
-    // Brave requires this to be present to enable the "Install" prompt
-    e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
