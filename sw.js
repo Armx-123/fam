4e65646cf755b84fc83cac05477be20971161341
Armx <armx94522@gmail.com>
2026-02-28 16:48:28 +0530

    Enhance fetch listener with a comment
    Added a comment to the fetch event listener for clarity.

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+    // A fetch listener is mandatory for browsers to enable the Install prompt
+    e.respondWith(fetch(e.request));
+});
