eee5e5ec09aa4c530e2e413725792c1ec2ef8ce6
Armx <armx94522@gmail.com>
2026-02-28 17:21:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-    // This allows the app to be installable by satisfying the 'offline-ready' check
-    event.respondWith(fetch(event.request));
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
