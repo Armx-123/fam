79a94a7fc132df907959d29dcf8fa365a3de0c04
Armx <armx94522@gmail.com>
2026-02-28 17:26:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
+const CACHE_NAME = 'fampay-cache-v1';
+const ASSETS = [
+  '/fam/',
+  '/fam/index.html',
+  '/fam/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
